d7e551cc8a4c7e36e889bdfd7bc081ea22d3887f
ManoharKonala <konalavrmanohar@gmail.com>
2025-05-30 12:57:35 +0530

    refactor(backend): improve User model comments and clarity

--- VoiceWave/backend/models/User.js
+++ VoiceWave/backend/models/User.js
@@ -1,90 +1,18 @@
+// User model for VoiceWave
+// Defines the user schema for authentication and social features
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
-      maxlength: 30,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-      select: false,
-    },
-    bio: {
-      type: String,
-      maxlength: 160,
-      default: '',
-    },
-    avatar: {
-      type: String,
-      default: '',
-    },
-    followers: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
-    following: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
-  },
-  { timestamps: true }
-);
-
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  avatar: { type: String },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  audios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Audio' }],
+  createdAt: { type: Date, default: Date.now }
 });
 
-// Method to compare passwords
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
+// Add more fields as needed for profile, stats, etc.
 
-// Virtual for getting follower/following counts
-userSchema.virtual('followerCount').get(function () {
-  return this.followers.length;
-});
-
-userSchema.virtual('followingCount').get(function () {
-  return this.following.length;
-});
-
-// Ensure virtuals are included when converting to JSON
-userSchema.set('toJSON', {
-  virtuals: true,
-  versionKey: false,
-  transform: function (doc, ret) {
-    delete ret.password;
-    delete ret.__v;
-    return ret;
-  },
-});
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
